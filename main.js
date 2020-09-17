98fb1e446f23aa96586c5de488eea28acb5bab3e
Bazaranol <romansidorkin2@gmail.com>
2020-09-17 14:20:54 +0700

    Fixed bug, add gallery with js file, & other stuff

---
+++ main.js
@@ -0,0 +1,11 @@
+// $("#addCard").on("click",function(){
+//     $(".card").clone().appendTo(".Exp");
+// });
+// $("p").click(function () {
+//     2
+//           $(this).clone().css('color','red').appendTo('h1');
+//     3
+//         });
+//     // $("button").click(function(){
+//     //     $(".card bg-dark text-white").clone().appendTo('.Exp');
+//     // });
---
+++ swiper.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper('.swiper-container1', {
+    slidesPerView: 4,
+    slidesPerColumn: 2,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+  
+  
+  var swiper = new Swiper('.swiper-container2', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
